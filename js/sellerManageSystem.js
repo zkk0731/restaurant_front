18f35906e3f5da79d6a9ae4b5458daa490bfb791
hao <you@exampl>
2022-12-15 17:33:36 +0800

    2nd commit

--- js/sellerManageSystem.js
+++ js/sellerManageSystem.js
@@ -16,3 +16,30 @@ $(document).on('click', '#showSalesVolumeBtn', function (e) {
     searchSalesVolume(startDateTime, endDateTime)
 })
 
+$(document).on('click', '#sendEmail', function (e) {
+    e.preventDefault()
+
+    let strEmailTitle = $('#emailTitle').val()
+    let strEmailMessage = $('#emailMessage').val()
+
+    sendEmail(strEmailTitle, strEmailMessage)
+})
+
+$(document).on('click', '#createPointsExchangeBtn', function (e) {
+    e.preventDefault()
+
+    let pointName = $('#pointName').val()
+    let pointCost = $('#pointCost').val()
+    let intDiscount = $('#pointDiscount').val()
+    createPointsExchange(pointName, pointCost, intDiscount)
+})
+
+$(document).on('click', '#createCommodityBtn', function (e) {
+    e.preventDefault()
+
+    let intPrice = $('#commodityPrice').val()
+    let strCommodityName = $('#commodityName').val()
+    let strCategory = $('#commodityCategory').val()
+
+    createCommodity(intPrice, strCommodityName, strCategory)
+})
